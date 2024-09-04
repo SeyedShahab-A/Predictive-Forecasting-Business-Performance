abc19ed098f05e7d3a44886d085adb8dce471c3f
Shahab Ashrafzadeh <45197230+SeyedShahab-A@users.noreply.github.com>
2024-09-04 23:10:39 +0100

    Preparation Part

---
+++ Preparation/Historical gather/modules/Cost_centre_structure.py
@@ -0,0 +1,24 @@
+# Cost_centre_structure.py
+CM_list = ['...']
+
+CM_sub_region = {...}
+
+INF_sub_region = {'...'}
+NR_sub_region = {'...'
+                }
+
+PM_sub_region = {'...'
+                 }
+
+ADV_sub_region = {'...'
+                  }
+
+AMCL_sub_region = {'...'
+                   }
+
+division_to_subregion = {
+}
+
+division_ab = {
+}
+
